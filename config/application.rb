3d430fe50aa22ffe75f4560aa66d8026bcba9ea0
EC2 Default User <ec2-user@ip-172-31-38-39.ap-southeast-1.compute.internal>
2019-02-11 08:57:04 +0000

    config.i18n.fallbacks = [I18n.default_locale]

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Matching
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
-    
+    config.i18n.fallbacks = [I18n.default_locale]
     initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
