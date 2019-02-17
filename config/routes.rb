48bfa63d4daef998d8878ffb9b1c9cdbd1297303
真砂祐貴 <kuronekoneko6@gmail.com>
2019-02-17 12:39:00 +0000

    update for upload to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  root  'users#index'
+  devise_scope :user do
+    root :to => "devise/sessions#new"
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users do
     member do
-     get :following, :followers
+    get :following, :followers
     end
   end
   resources :relationships, only: [:create, :destroy]
