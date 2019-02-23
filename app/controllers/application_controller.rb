241a20ed230e927028e7df755e4a7ac5325103d3
真砂祐貴 <kuronekoneko6@gmail.com>
2019-02-23 09:14:27 +0000

    update for upload to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :profile, :avatar])
   end
-  # def after_sign_in_path_for(resource)
-  #   '/users'
-  # end
+  def after_sign_in_path_for(resource)
+    '/users'
+  end
 end
